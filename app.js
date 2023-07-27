6bb9a7bd73399a6d39daeabd5afc70da6c94bb83
saulpadilla10 <saulp1021@yahoo.com>
2023-07-27 00:10:53 -0700

    JS Variables

--- app.js
+++ app.js
@@ -4,7 +4,40 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Saul";
+let lastName;
+var age;
 
 // Exercise 2
+firstName;
+lastName = "Padilla";
+age = 30;
+
+console.log(`First Name: ${firstName} 
+Last Name: ${lastName} 
+Age: ${age}`);
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(isCaseSensitive + " " + language + " " + "created in:" + createdYear)
+
+let price = 19.99;
+let isOnSale = false;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`Item Price:${price} On Sale: ${isOnSale} Stock=${stock} 
+Size ${selectedSize} in stock: ${inStock}`)
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} by ${author} has a page count of ${pageCount}
+bookmark is on page ${bookmark} Been read? ${hasRead}`)
